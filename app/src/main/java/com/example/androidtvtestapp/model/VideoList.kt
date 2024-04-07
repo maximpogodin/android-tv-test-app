2ae77835d5602b37ea706cbd7bac9be410446cc7
maximpogodin <maxim.pogodin2011@yandex.ru>
2024-04-07 19:51:17 +0500

    reload

--- app/src/main/java/com/example/androidtvtestapp/model/VideoList.kt
+++
@@ -1,7 +0,0 @@
-package com.example.androidtvtestapp.model
-
-data class VideoList(
-    val error : Int,
-    val videos : List<Video>,
-    val count : Int
-)
--- app/src/main/java/com/example/androidtvtestapp/network/ExternalApiOptions.kt
+++
@@ -1,6 +0,0 @@
-package com.example.androidtvtestapp.network
-
-object ExternalApiOptions {
-    const val BASE_URL = "PASTE_YOUR_URL_HERE"
-    const val API_KEY = "PASTE_YOUR_API_KEY_HERE"
-}
